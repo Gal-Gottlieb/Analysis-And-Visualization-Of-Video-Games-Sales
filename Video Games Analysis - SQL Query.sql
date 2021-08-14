719b3733863ca58e7c67cca36b1cd59748a77440
Gal Gottlieb <78704632+Gal-Gottlieb@users.noreply.github.com>
2021-08-14 10:41:41 -0300

    Update Video Games Analysis - SQL Query.sql

--- Video Games Analysis - SQL Query.sql
+++ Video Games Analysis - SQL Query.sql
@@ -15,7 +15,7 @@ as 'Sales - 90thPerc'
 ,round(max(Global_Sales) over (partition by genre.genre)*1000000,0)  as 'Max Sales'
 from dbo.[Fact_Sales_2$] as sales inner join Dim_Genre$ as Genre
 on genre.[Genre ID]=sales.Genre_ID
-order by genre.Genre
+order by genre.Genre ;
 
 
 --- 2 - Creating general table of the top 10% of games by their global sales
@@ -30,7 +30,7 @@ on pub.Publisher_ID=sales.Publisher_ID inner join Dim_Genre$ as gen
 on gen.[Genre ID]=sales.Genre_ID
 where sales.Global_Sales*1000000 > (select distinct PERCENTILE_CONT(0.9) within group (order by Global_Sales) over (partition by null)*1000000
 from dbo.[Fact_Sales_2$] as sales inner join Dim_Genre$ as Genre
-on sales.Genre_ID=genre.[Genre ID])
+on sales.Genre_ID=genre.[Genre ID]) ;
 
 
 /*
@@ -57,7 +57,7 @@ where Year_of_Release between 2000 and 2016
 group by gen.[Genre ID],gen.Genre
 ) as Q2
 on Q1.[Genre ID]=Q2.[Genre ID] and Q1.Genre=Q2.Genre
-order by '% Of Genre Games In Top 10%' desc
+order by '% Of Genre Games In Top 10%' desc ;
 
 /*
 5 - Creating table that shows by platform the number of games in top 10% by total sales and percent of games by genre in top 10%
@@ -85,4 +85,6 @@ group by plat.[Platform ID],plat.Platform
 Having count(*)>100
 ) as Q4
 on Q3.[Platform ID]=Q4.[Platform ID] and Q3.platform=Q4.Platform
-order by "% Of Platform's Games In Top 10%" desc
+order by "% Of Platform's Games In Top 10%" desc ;
+
+END
